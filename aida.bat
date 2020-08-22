d86c4bc361c1c625b2d87f460a12af4728a38a1a
Akash Mittal <akashmittal18@yahoo.com>
2020-08-22 22:53:41 +0530

    BAT file

---
+++ aida.bat
@@ -0,0 +1,14 @@
+ echo off
+title AIDA
+echo /AIDA AI
+echo                ___       __   _______       ___
+echo               /   \     !  ! !       \     /   \
+echo              /  ^  \     !  ! !  .--.  !   /  ^   \
+echo             /  /_\  \   !  ! !  !  !  !  /  /_\  \
+echo            /  _____  \  !  ! !  '--'  ! /  _____  \
+echo           /__/     \__\ !__! !_______/ /__/     \__\
+
+
+echo  HOW MAY I HELP YOU BOSS press Enter key
+
+pause > nul
